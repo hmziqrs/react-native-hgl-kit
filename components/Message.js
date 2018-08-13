4a7cfd2975366ff8a27a8ddd39f2fa45b6f8599f
hackerhgl <hackerhgl@gmail.com>
2018-08-13 21:56:37 +0500

    messages

---
+++ components/Message.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * Message
+ *
+ */
+
+import React from 'react';
+import { Text, Animated } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { injectIntl, intlShape } from 'react-intl'; //eslint-disable-line
+
+function Message({
+    intl, id, defaultMessage, values, animated, ...props
+}) {
+    let Comp = Text;
+    if (animated) {
+        Comp = Animated.Text;
+    }
+    return (
+        <Comp {...props}>
+            {intl.formatMessage({ id, defaultMessage }, values)}
+        </Comp>
+    );
+}
+
+Message.propTypes = {
+    intl: intlShape.isRequired,
+    id: PropTypes.string.isRequired,
+    defaultMessage: PropTypes.string.isRequired,
+    values: PropTypes.object,
+    animated: PropTypes.bool,
+};
+
+Message.defaultProps = {
+    values: {},
+    animated: false,
+};
+
+export default injectIntl(Message);
